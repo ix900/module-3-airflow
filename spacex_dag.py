fb0f7aab5c8b03635211fd3764b997e98a3ed4b9
ix900 <ix900@mail.ru>
2021-02-24 09:50:54 +0300

    Update spacex_dag.py

--- spacex_dag.py
+++ spacex_dag.py
@@ -16,17 +16,22 @@ default_args = {
 
 dag = DAG("spacex", default_args=default_args, schedule_interval="0 0 1 1 *")
 
-t1 = BashOperator(
-    task_id="get_data", 
-    bash_command="python3 /root/airflow/dags/spacex/load_launches.py -y {{ execution_date.year }} -o /var/data", 
-    dag=dag
-)
+prm = {}
+for rocket_type in ["all", "falcon1", "falcon9", "falconheavy"]:            
+    prm.update({'rocket': rocket_type })
+    t1 = BashOperator(
+        task_id="get_data", 
+        bash_command="python3 /root/airflow/dags/spacex/load_launches.py -y {{ execution_date.year }} -o /var/data {}".format("-r {{ params.rocket }}" if rocket_type!="all" else ""), 
+        params=prm,
+        dag=dag
+        
+    )
 
-t2 = BashOperator(
-    task_id="print_data", 
-    bash_command="cat /var/data/year={{ execution_date.year }}/rocket={{ params.rocket }}/data.csv", 
-    params={"rocket": "all"}, # falcon1/falcon9/falconheavy
-    dag=dag
-)
+    t2 = BashOperator(
+        task_id="print_data", 
+        bash_command="cat /var/data/year={{ execution_date.year }}/rocket={{ params.rocket }}/data.csv", 
+        params=prm, # falcon1/falcon9/falconheavy
+        dag=dag
+    )
 
-t1 >> t2
+    t1 >> t2
